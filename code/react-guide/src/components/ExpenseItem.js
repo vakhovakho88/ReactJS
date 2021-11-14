07bcaf974846f034c12b216d0a21465fb330458e
menabdishviliva67919 <vakho.menabdishvili@gmail.com>
2021-11-14 19:29:20 +0100

    first commitment

---
+++ code/react-guide/src/components/ExpenseItem.js
@@ -0,0 +1,26 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  const expenseTitle = props.title;
+  const expenseAmount = props.amount;
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
