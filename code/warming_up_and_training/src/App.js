eddd08d1a233fe5c8d49b6a20c80fddedcbaff08
vakhovakho88 <vakho.menabdishvili@gmail.com>
2023-11-18 13:47:51 +0100

    My first training app

---
+++ code/warming_up_and_training/src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+
+// Define an array of people data with their names and ages
+const peopleData = [
+  { name: "John", age: 25 },
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 22 },
+  { name: "Eve", age: 35 },
+  { name: "Charlie", age: 28 },
+  { name: "Olivia", age: 29 },
+  { name: "Daniel", age: 33 },
+  { name: "Sophia", age: 27 },
+  { name: "Liam", age: 31 },
+  { name: "Emma", age: 24 },
+  { name: "Michael", age: 26 },
+  { name: "Ava", age: 32 },
+  { name: "William", age: 29 },
+  { name: "Mia", age: 23 },
+  { name: "James", age: 34 },
+  { name: "Isabella", age: 28 },
+  { name: "Benjamin", age: 30 },
+  { name: "Sophia", age: 25 },
+  { name: "Lucas", age: 27 },
+  { name: "Charlotte", age: 31 },
+];
+
+
+function App() {
+  return (
+    <div>
+      {/* Map over the peopleData array to render a list of paragraphs */}
+      {peopleData.map((person, index) => (
+        // Each paragraph has a unique 'key' attribute to help React efficiently update the list
+        <p key={index}>
+           {/* Display the person's name and age */}
+          Hi My Name is <b>{person.name}</b> and my age is <b>{person.age}</b>
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
